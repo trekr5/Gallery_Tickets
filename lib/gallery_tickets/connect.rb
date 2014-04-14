20590a2c153f6d769f9f42ab6999b1b7d42bd61a
Teja Kocjancic <teja.kocjancic@fundingcircle.com>
2014-04-14 15:46:58 +0100

    some more tests

--- lib/gallery_tickets/connect.rb
+++ lib/gallery_tickets/connect.rb
@@ -12,20 +12,20 @@ module GalleryTickets
     end 
 
     def self.single_gallery
-      # opens the connection to the api
+      
     response = HTTParty.get("http://arts-api.herokuapp.com/galleries/1")
     puts response
 
-      # returns a list of galleries
+     
     JSON.parse response.body
     end 
 
     def self.exhibitions
-      # opens the connection to the api
+     
     response = HTTParty.get("http://arts-api.herokuapp.com/galleries/1/exhibitions")
     puts response
 
-      # returns a list of galleries
+     
     JSON.parse response.body
     end 
 
