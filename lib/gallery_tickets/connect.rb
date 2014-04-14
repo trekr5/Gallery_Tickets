8508a355099b40dac168ddae8492c7a531fd6e11
Teja Kocjancic <teja.kocjancic@fundingcircle.com>
2014-04-14 15:43:38 +0100

    new tests

--- lib/gallery_tickets/connect.rb
+++ lib/gallery_tickets/connect.rb
@@ -3,8 +3,29 @@ module GalleryTickets
   class Connect
 
     def self.galleries
-    response = HTTParty.get("http://arts-api.herokuapp.com/galleries&format=json")
+      # opens the connection to the api
+    response = HTTParty.get("http://arts-api.herokuapp.com/galleries")
     puts response
+
+      # returns a list of galleries
+    JSON.parse response.body
+    end 
+
+    def self.single_gallery
+      # opens the connection to the api
+    response = HTTParty.get("http://arts-api.herokuapp.com/galleries/1")
+    puts response
+
+      # returns a list of galleries
+    JSON.parse response.body
+    end 
+
+    def self.exhibitions
+      # opens the connection to the api
+    response = HTTParty.get("http://arts-api.herokuapp.com/galleries/1/exhibitions")
+    puts response
+
+      # returns a list of galleries
     JSON.parse response.body
     end 
 
--- spec/api_test_spec.rb
+++ spec/api_test_spec.rb
@@ -25,4 +25,34 @@ describe GalleryTickets::Connect do
     end
   end
 
+  describe '#single_gallery' do
+
+    let(:response) { double('response', body: '{"name": "Hayward"}')}
+
+    it "make request to the right end point" do
+      HTTParty.should_receive(:get, :with => 'http://arts-api.herokuapp.com/galleries/1').and_return(response)
+      GalleryTickets::Connect.single_gallery
+    end
+
+    it "returns information for a specific gallery" do
+      HTTParty.stub(get: response)
+      GalleryTickets::Connect.single_gallery.should eq('name' => "Hayward")
+    end
+  end
+
+  describe '#exhibitions' do
+
+    let(:response) { double('response', body: '{"name": "Futurism And You"}')}
+
+    it "make request to the right end point" do
+      HTTParty.should_receive(:get, :with => 'http://arts-api.herokuapp.com/galleries/1/exhibitions').and_return(response)
+      GalleryTickets::Connect.exhibitions
+    end
+
+    it "lists the exhibitions" do
+      HTTParty.stub(get: response)
+      GalleryTickets::Connect.exhibitions.should eq('name' => "Futurism And You")
+    end
+  end
+
 end
